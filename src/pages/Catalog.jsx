2fcff30eb5b3cba564ab5ed914ef25b6277ed3b0
Ivan Vasil <ivansolontsov@yandex.ru>
2023-02-27 22:23:36 +0400

    new photo added

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import '../css/catalog/catalog.scss';
 import { CatalogItem } from '../components/catalog/CatalogItem'
 import HowToOrder from '../components/HowToOrder'
-import nozhik from '../assets/images/catalog/nozhi.png'
+import knife1 from '../assets/images/catalog/1.webp'
+import knife2 from '../assets/images/catalog/2.webp'
+import knife3 from '../assets/images/catalog/3.webp'
+import knife4 from '../assets/images/catalog/4.webp'
+import knife5 from '../assets/images/catalog/5.webp'
+import knife6 from '../assets/images/catalog/6.webp'
+import knife7 from '../assets/images/catalog/7.webp'
+import knife8 from '../assets/images/catalog/8.webp'
+import knife9 from '../assets/images/catalog/9.webp'
+import knife10 from '../assets/images/catalog/10.webp'
+import knife11 from '../assets/images/catalog/11.webp'
 import noPhoto from '../assets/images/catalog/noPhoto.webp'
 import DocumentMeta from 'react-document-meta'
 import { ContactUs } from '../components/ContactUs'
@@ -32,32 +42,34 @@ const Catalog = () => {
   ]
 
   let catalog = [
-    { id: 1, image: nozhik, title: 'Нож противорежущий для комбайнов Claas', l: '190', b: '50', s: '4', d: '10', price: 400, category: 0, combine: [1] },
-    { id: 2, image: nozhik, title: 'Нож противорежущий для комбайнов CASE 1322233C2, 51011.1', l: '175', b: '40', s: '4', d: '11', price: 330, category: 0, combine: [2] },
-    { id: 3, image: nozhik, title: 'Нож противорежущий для комбайнов Tucano, Mega, Lexion 060030.0 (42253)', l: '173', b: '50', s: '4', d: '20', price: 330, category: 0, combine: [3, 4, 5] },
-    { id: 4, image: nozhik, title: 'Нож противорежущий для комбайнов', l: '190', b: '50', s: '5', d: '20', price: 550, category: 0, combine: [] },
-    { id: 5, image: nozhik, title: 'Нож противорежущий для комбайнов New Holland', l: '170', b: '50', s: '5', d: '20', price: 400, category: 0, combine: [6] },
-    { id: 6, image: nozhik, title: 'Нож противорежущий для комбайнов Акрос, Вектор, Торум 42228.10', l: '173', b: '50', s: '4', d: '18', price: 330, category: 0, combine: [7, 8, 9] },
-    { id: 7, image: nozhik, title: 'Нож противорежущий для комбайнов CLAAS 2166/2388/5088/7088', l: '125', b: '95', s: '6', d: '14x2', price: 825, category: 0, combine: [1] },
-    { id: 8, image: nozhik, title: 'Нож противорежущий для комбайнов CASE 5102401 1994760C4', l: '155', b: '50', s: '5', d: '20', price: 825, category: 0, combine: [2] },
-    { id: 9, image: nozhik, title: 'Нож противорежущий для комбайнов Lexion 460', l: '220', b: '60', s: '5', d: '20', price: 825, category: 0, combine: [5] },
-    { id: 11, image: noPhoto, title: '127459A1 Граблина', price: 299, category: 1, },
-    { id: 12, image: noPhoto, title: 'Подшипник деревянный (бук)', price: 299, category: 1, },
-    { id: 13, image: noPhoto, title: 'Натяжитель деревянный (бук)', price: 299, category: 1, },
-    { id: 14, image: noPhoto, title: 'Вал Ходового', price: 299, category: 1, },
-    { id: 15, image: noPhoto, title: 'Вал Тормозного', price: 299, category: 1, },
-    { id: 16, image: noPhoto, title: 'Вал Зернового', price: 299, category: 1, },
-    { id: 17, image: noPhoto, title: 'Вал Колосового 19178C1', price: 299, category: 1, },
-    { id: 18, image: noPhoto, title: 'Вал под игольчатый подшипник', price: 299, category: 1, },
-    { id: 19, image: noPhoto, title: 'Шкварень', price: 299, category: 1, },
-    { id: 20, image: noPhoto, title: 'Муфта Ветров', price: 299, category: 1, },
-    { id: 21, image: noPhoto, title: 'Муфта ГСТ', price: 299, category: 1, },
-    { id: 22, image: noPhoto, title: 'Муфта Ходового Вала', price: 299, category: 1, },
-    { id: 23, image: noPhoto, title: 'Палец Консульного Шнека', price: 299, category: 1, },
-    { id: 24, image: noPhoto, title: 'Вал Звездочка', price: 299, category: 1, },
-    { id: 25, image: noPhoto, title: 'Натяжник', price: 299, category: 1, },
-    { id: 26, image: noPhoto, title: 'Хомут Мотовило', price: 299, category: 1, },
-    { id: 27, image: noPhoto, title: 'Тара Упаковочная Деревянная (Ящики)', price: 299, category: 2, },
+    { image: knife1, title: 'Нож противорежущий для комбайнов Claas', l: '190', b: '50', s: '4', d: '12', price: 400, category: 0, combine: [1] },
+    { image: knife2, title: 'Нож противорежущий для комбайнов CASE 1322233C2, 51011.1', l: '175', b: '40', s: '4', d: '11', price: 330, category: 0, combine: [2] },
+    { image: knife3, title: 'Нож противорежущий для комбайнов Tucano, Mega, Lexion 060030.0 (42253)', l: '173', b: '50', s: '4', d: '20', price: 330, category: 0, combine: [3, 4, 5] },
+    { image: knife4, title: 'Нож противорежущий для комбайнов', l: '190', b: '50', s: '4', d: '20', price: 330, category: 0, combine: [] },
+    { image: knife5, title: 'Нож противорежущий для комбайнов New Holland', l: '170', b: '50', s: '5', d: '20', price: 400, category: 0, combine: [6] },
+    { image: knife6, title: 'Нож противорежущий для комбайнов Акрос, Вектор, Торум 42228.10', l: '173', b: '50', s: '4', d: '18', price: 330, category: 0, combine: [7, 8, 9] },
+    { image: knife7, title: 'Нож противорежущий для комбайнов CLAAS 2166/2388/5088/7088', l: '125', b: '95', s: '6', d: '14x2', price: 825, category: 0, combine: [1] },
+    { image: knife8, title: 'Нож противорежущий для комбайнов CASE 5102401 1994760C4', l: '155', b: '50', s: '5', d: '20', price: 825, category: 0, combine: [2] },
+    { image: knife9, title: 'Нож противорежущий для комбайнов CASE', l: '150', b: '60', s: '6', d: '11', price: 660, category: 0, combine: [5] },
+    { image: knife10, title: 'Нож противорежущий для комбайнов Lexion 460', l: '220', b: '60', s: '5', d: '20', price: 750, category: 0, combine: [5] },
+    { image: knife11, title: 'Нож противорежущий для комбайнов CASE 1994760С4', price: 550, category: 0, combine: [2] },
+    { image: noPhoto, title: '127459A1 Граблина', price: 299, category: 1, },
+    { image: noPhoto, title: 'Подшипник деревянный (бук)', price: 299, category: 1, },
+    { image: noPhoto, title: 'Натяжитель деревянный (бук)', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал Ходового', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал Тормозного', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал Зернового', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал Колосового 19178C1', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал под игольчатый подшипник', price: 299, category: 1, },
+    { image: noPhoto, title: 'Шкварень', price: 299, category: 1, },
+    { image: noPhoto, title: 'Муфта Ветров', price: 299, category: 1, },
+    { image: noPhoto, title: 'Муфта ГСТ', price: 299, category: 1, },
+    { image: noPhoto, title: 'Муфта Ходового Вала', price: 299, category: 1, },
+    { image: noPhoto, title: 'Палец Консульного Шнека', price: 299, category: 1, },
+    { image: noPhoto, title: 'Вал Звездочка', price: 299, category: 1, },
+    { image: noPhoto, title: 'Натяжник', price: 299, category: 1, },
+    { image: noPhoto, title: 'Хомут Мотовило', price: 299, category: 1, },
+    { image: noPhoto, title: 'Тара Упаковочная Деревянная (Ящики)', price: 299, category: 2, },
   ]
 
   const meta = {
@@ -88,7 +100,6 @@ const Catalog = () => {
             )
           })
         }
-
         <HowToOrder />
         <ContactUs isOrder={true} />
       </div>
